70d7f6d40cb02dcb30744e3fa9057dd697054fbb
YurGoV <yurgov@gmail.com>
2022-10-20 23:05:45 +0300

    pagination_added

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,75 +1,151 @@
 
 // const axios = require('axios');
 const axios = require('axios').default;
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+Notify.init({
+  width: notifySetup().width,
+  position: notifySetup().position,
+  distance: '10px',
+  opacity: 0.7,
+  clickToClose: true,
+  fontSize: notifySetup().fontSize,
+});
 
 const ref = {
   searchButton: document.querySelector('.search-form'),
   gallerySet: document.querySelector('.gallery'),
+  nextPageButton: document.querySelector('button.load-more'),
 }
 
 console.log(ref.searchButton);
 console.log(ref.gallerySet);
+console.log(ref.nextPageButton);
 
-const querry = ref.searchButton.addEventListener('submit', getPictures);
-const clk = ref.gallerySet.addEventListener('click', onClick);
+const onSearchButton = ref.searchButton.addEventListener('submit', onSearchButtonClick);
+const onImage = ref.gallerySet.addEventListener('click', onImageClick);
+const onNextPageButton = ref.nextPageButton.addEventListener('click', onNextPageButtonClick)
 
-function onClick(event) {
+function onImageClick(event) {
   console.log(event.target.alt);
 }
 
+function onNextPageButtonClick(event) {
+  console.log('search');
+  fingImagesService.setPage();
+  fingImagesService.find();
+}
 
-// // ===
+// ===
 // const picturesSearch = {
 
 //   onSearch
 
 
-
-
-
-
 // }
 
+const fingImagesService = {
 
-function getPictures(event) {
-  event.preventDefault();
-  // console.log('ddd');
-  console.log(event.currentTarget.elements.searchQuery.value);
-  const querryString = event.currentTarget.elements.searchQuery.value;
+  page: 1,
+  querryString: '',
+  previousSearch: '',
+  prevoiusPage: NaN,
 
-  const querry = axios.get('https://pixabay.com/api/', {
+
+  find () {
+
+    
+  if (this.querryString === '') {
+    return Notify.failure(`please input what you want to search!`);
+  }
+
+  console.log(this.querryString);
+
+  if (this.previousSearch !== '' && this.previousSearch !== this.querryString) {
+    console.log('!==');
+    this.page = 1;
+    clearGallery();
+  }
+
+  if (this.prevoiusPage === this.page && this.previousSearch === this.querryString) {
+    return Notify.success(`To show more press "Load more" downside button or change search querryy please`);
+  }
+    
+  // this.querryString = ''
+
+    const querry = axios.get('https://pixabay.com/api/', {
       params: {
         key:'30695501-7cf0afb8f69a77a083ed747e6',
-        q: querryString,
+        q: this.querryString,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
-        per_page: 40
+        page: this.page,
+        per_page: 40,
       }
     })
     .then(function (response) {
       onGetValidData(response)
-    //   // return response;
-    //   console.log(response.data.totalHits);
-    //   console.log(response.data.hits);
     })
-    // .then(onGetValidData(event))
-    // // .catch(function (error) {
-    // //   console.log(error);
-    // // })
+
     .then(function () {
       // always executed
     });
 
-    console.log(typeof querry);
+    console.log(`displaying page: ${this.page}`);
+    console.log(`displaying prevPage: ${this.prevoiusPage}`);
+    console.log(`searchin QS: ${this.querryString}`);
+    console.log(`previous QS: ${this.previousSearch}`);
+
+    // this.querryString = querryString;
+    // this.saveQuerryString()
+    this.previousSearch = this.querryString;
+    this.prevoiusPage = this.page;
+
+  },
+
+  setPage () {
+    this.page += 1;
+    console.log(this.page);
+  },
+
+}
+
+
+
+// console.log(typeof querry);
+
+
+
+
+function onSearchButtonClick(event) {
+  event.preventDefault();
+  
+  console.log(event.currentTarget.elements.searchQuery.value);
+  const querryString = event.currentTarget.elements.searchQuery.value;
+
+    // fingImagesService.find(querryString);
+    fingImagesService.querryString = querryString;
+  fingImagesService.find();
+
+
+
 };
 
 
 
 function onGetValidData(dataArray) {
-  console.log(dataArray);
-  console.log(dataArray.data.totalHits);
+  // console.log(dataArray);
+  const totalHits = dataArray.data.totalHits;
+
+  if (totalHits === 0) {
+    console.log(totalHits);
+    return Notify.failure(`Sorry, there are no images matching your search querry. Please try again`);
+  };
+
+  Notify.success(`Horray! We found ${totalHits} images.`);
+  // console.log(dataArray);
   // ttt = dataArray.data.hits;
+
   const stringToDisplay = dataArray.data.hits.map(
     ({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => 
   `<div class="photo-card">
@@ -100,20 +176,37 @@ function onGetValidData(dataArray) {
   )
   .join('');
 
-  ref.gallerySet.insertAdjacentHTML('beforeend', stringToDisplay)
-  // console.log(stringToDisplay);
-  
-  
- //array of pictures
+  ref.gallerySet.insertAdjacentHTML('beforeend', stringToDisplay);
+  // ref.gallerySet.innerHTML = stringToDisplay;
 
 }
+
+function clearGallery() {
+  ref.gallerySet.innerHTML = '';
+}
+
+function notifySetup() {
+  if (window.innerWidth >= 1100) {
+      return {
+      position: 'center-top',
+      fontSize: '18px',
+      width: '400px',
+      }
+  };
+  return {
+  fontSize: '14px',
+  position: 'right-top',
+  width: '280px',
+  };
+};
+    // console.log(notifySetup().fontSize);
 
 // access to large image link:  /listener/dataset.largeImage
 
 
-// getPictures('yellow+flowers');
+// onImageClick('yellow+flowers');
 
-// async function getPictures(request) {
+// async function onImageClick(request) {
 //   event.preventDefault()
 //   try {
 //     request.preventDefault()
@@ -135,7 +228,7 @@ function onGetValidData(dataArray) {
 //   }
 // }
 
-// getPictures('yellow+flowers');
+// onImageClick('yellow+flowers');
 
 
 
@@ -178,7 +271,7 @@ function onGetValidData(dataArray) {
 
 
 // const searchButton = document.querySelector('.search-form');
-// const querry = searchButton.querySelector('submit', getPictures);
+// const querry = searchButton.querySelector('submit', onImageClick);
 
 
 // const querry = axios.get('https://pixabay.com/api/', {
